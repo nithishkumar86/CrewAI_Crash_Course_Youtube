a137f44ee351e42651aff0e05bc2e522df597763
unknown <mnithish1231234@gmail.com>
2026-02-16 15:21:43 +0530

    task update

--- Tasks/src/Config_parameter/main_crew.py
+++ Tasks/src/Config_parameter/main_crew.py
@@ -1,9 +1,19 @@
+# in crewai two ways we can able to create the agents and tasks
+# 1).normal way Agent,Task we directly design agents
+# 2).using yaml file we can create agent and task in documentation 
+# they are suggesting to use this technique
+
 # src/latest_ai_development/crew.py
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 from crewai import Agent, Crew, Process, Task,LLM
 from crewai.project import CrewBase, agent, crew, task
 from crewai_tools import SerperDevTool
 
+# The names you use in your YAML files (agents.yaml and tasks.yaml) 
+# should match the method names in your Python code.
 
 @CrewBase
 class LatestAiDevelopmentCrew():
